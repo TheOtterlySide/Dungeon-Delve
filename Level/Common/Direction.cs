6599b34d3199ff185057fdcb6156efb6acbf3244
The Otterly Side <47543964+TheOtterlySide@users.noreply.github.com>
2026-02-08 23:17:11 +0100

    add: Directions

---
+++ Level/Common/Direction.cs
@@ -0,0 +1,9 @@
+namespace DungeonDelve.Level.Common;
+
+public enum Direction
+{
+    North, // -Z
+    East,  // +X
+    South, // +Z
+    West   // -X
+}
--- Rooms/SPECIAL/RoomSocket.cs
+++ Rooms/SPECIAL/RoomSocket.cs
@@ -1,9 +1,12 @@
 using Godot;
 using System;
+using DungeonDelve.Level.Common;
 
 public partial class RoomSocket : Marker3D
 {
+    private Direction Direction;
     public bool isUsed;
+    
     public void Use()
     {
         if (isUsed)
@@ -12,6 +15,30 @@ public partial class RoomSocket : Marker3D
         }
 
         isUsed = true;
+    }
+
+    public Direction GetDirection()
+    {
+        var globalPosition = GlobalPosition;
+
+        if (Mathf.Abs(globalPosition.X) > Mathf.Abs(globalPosition.Z))
+        {
+            return globalPosition.X > 0 ? Direction.West : Direction.East;
+        }
+
+        return globalPosition.Z > 0 ? Direction.South : Direction.North;
+    }
+    
+    public Direction GetOpposite(Direction dir)
+    {
+        return dir switch
+        {
+            Direction.North => Direction.South,
+            Direction.South => Direction.North,
+            Direction.East  => Direction.West,
+            Direction.West  => Direction.East,
+            _ => dir
+        };
     }
     
 }
