6bf21dd4a819e68ff4f7f56a184c89cc6b570198
The Otterly Side <47543964+TheOtterlySide@users.noreply.github.com>
2026-08-20 20:24:18 +0200

    add: Tabsssss

--- Level/Common/Enum/DebugCategory.cs
+++ Level/Common/Enum/DebugCategory.cs
@@ -2,21 +2,7 @@ namespace DungeonDelve.Level.Common.Enum;
 
 public enum DebugCategory
 {
-    Player,
-    Enemy,
-    Item,
     Environment,
-    UI,
-    Physics,
-    Audio,
-    Network,
-    AI,
-    Animation,
-    Input,
-    Scripting,
-    Performance,
-    ResourceManagement,
-    SaveLoadSystem,
     Interactable,
     Inventory,
 }
--- Level/Manager/DebugManager.cs
+++ Level/Manager/DebugManager.cs
@@ -1,12 +1,18 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using DungeonDelve.Level.Common.Enum;
 
 public partial class DebugManager : Node
 {
     public static DebugManager Instance { get; private set; }
     private RichTextLabel _console;
+    private TabBar _tabBar;
     public bool DebugMode;
+    
+    private Dictionary<int, (DebugCategory, string)> _categoryLogs = new Dictionary<int, (DebugCategory, string)>();
+    private List<string> _logMessages = new List<string>();
+    private int _logid = 0;
 
     public override void _EnterTree()
     {
@@ -16,6 +22,15 @@ public partial class DebugManager : Node
     public override void _Ready()
     {
         _console = Instance.GetNode<RichTextLabel>("Console");
+        _tabBar = Instance.GetNode<TabBar>("TabBar");
+        
+        foreach (var category in Enum.GetValues<DebugCategory>())
+        {
+            if (_tabBar.GetTabCount() >= Enum.GetValues<DebugCategory>().Length) continue;
+            _tabBar.AddTab(category.ToString());
+        }
+        _tabBar.TabChanged += OnTabChanged;
+        
     }
 
     public override void _Process(double delta)
@@ -45,18 +60,45 @@ public partial class DebugManager : Node
     private void Log(string message, DebugCategory category)                                                                      
     {
         GD.Print($"[Debug] {message}");
-        _console.AppendText($"[Debug] {message}\n");
+        _categoryLogs.Add(_logid++,(category, message));
+        SendToConsole();
     }
 
     private void LogError(string message, DebugCategory category)
     {
         GD.PrintErr($"[Error] {message}");
-        _console.AppendText($"[Error] {message}\n");
+        _categoryLogs.Add(_logid++,(category, message));
+
+
+        SendToConsole();
     }
     
     private void LogWarning(string message, DebugCategory category)
     {
         GD.PrintRich($"[color=yellow] [Warning] {message} [/color]");
-        _console.AppendText($"[Warning] {message}\n");
+        _categoryLogs.Add(_logid++,(category, message));
+
+
+        SendToConsole();
+    }
+    
+    private void BuiltTabBar(TabBar tabBar)
+    {
+     
+    }
+
+    private void OnTabChanged(long tab)
+    {
+        var selectedCategory = (DebugCategory)tab;
+        GD.Print($"Selected Debug Category: {selectedCategory}");
+    }
+    
+    private void SendToConsole()
+    {
+        _console.Clear();
+        foreach(var message in _categoryLogs)
+        {
+            _console.AppendText(message.Value + "\n");
+        }
     }
 }
